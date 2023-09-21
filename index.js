68ecc3680d0803f03b511ed34c65c211ef7235e2
nelson <neto.nelson99@gmail.com>
2023-09-21 10:44:54 -0300

    Subindo pasta para o repositorio

---
+++ index.js
@@ -0,0 +1,14 @@
+button = document.getElementById('botao').addEventListener('click', function(){
+    
+    let nome = document.getElementById('nome-pokemon').value
+
+    fetch('https://pokeapi.co/api/v2/pokemon/' +nome)
+    .then(function(response){
+        return response.json()
+    }).then(function(response){
+        console.log(response.sprites.front_default)
+
+        let url = response.sprites.front_default
+        document.getElementById('img-card').setAttribute('src', url)
+    })
+})
