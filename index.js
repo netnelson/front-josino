e457149a1e3193ffca7ac7f45298c42a236fa077
nelson <neto.nelson99@gmail.com>
2023-09-21 11:57:02 -0300

    aula 21/09

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ button = document.getElementById('botao').addEventListener('click', function(){
     .then(function(response){
         return response.json()
     }).then(function(response){
+        
+        let html_habilidades = ''
+        for (const hab of response.abilities){
+            html_habilidades = '<li>'+hab.ability.name+'</li>'
+        }
+        let lista = document.getElementById('habilidades')
+        lista.innerHTML = html_habilidades
+
         console.log(response.sprites.front_default)
 
         let url = response.sprites.front_default
